3bcd9cb83ace50da313fd29b8babae7d1a63b231
adithyavenkatesh <adithyav211@gmail.com>
2020-11-08 17:18:18 -0500

    added labels

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,27 @@
-var chosenXAxis = "poverty";
-var chosenYAxis = "healthcare";
+var xaxis = "poverty";
+var yaxis = "healthcare";
+
+var margin = { top: 20, right: 20, bottom: 50, left: 70 },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 // Function used for updating x-scale var upon click on axis label.
-function xScale(data, chosenXAxis, chartWidth) {
+function xScale(data, xaxis, chartWidth) {
     // Create scales.
     var xLinearScale = d3.scaleLinear()
-        .domain([d3.min(data, d => d[chosenXAxis]) * .8,
-            d3.max(data, d => d[chosenXAxis]) * 1.1
+        .domain([d3.min(data, d => d[xaxis]) * .8,
+            d3.max(data, d => d[xaxis]) * 1.1
         ])
         .range([0, chartWidth]);
     return xLinearScale;
 }
 
 // Function used for updating y-scale var upon click on axis label.
-function yScale(data, chosenYAxis, chartHeight) {
+function yScale(data, yaxis, chartHeight) {
     // Create scales.
     var yLinearScale = d3.scaleLinear()
-        .domain([d3.min(data, d => d[chosenYAxis]) * .8,
-            d3.max(data, d => d[chosenYAxis]) * 1.2
+        .domain([d3.min(data, d => d[yaxis]) * .8,
+            d3.max(data, d => d[yaxis]) * 1.2
         ])
         .range([chartHeight, 0]);
     return yLinearScale;
@@ -35,13 +39,6 @@ function scatterPlot() {
     //SVG params.
     var svgHeight = window.innerHeight / 1.2;
     var svgWidth = window.innerWidth / 1.7;
-    // Margins.
-    var margin = {
-        top: 50,
-        right: 50,
-        bottom: 100,
-        left: 80
-    };
 
     // Chart area minus margins.
     var chartHeight = svgHeight - margin.top - margin.bottom;
@@ -57,7 +54,7 @@ function scatterPlot() {
 
     // Append an SVG group
     var chartGroup = svg.append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
     d3.csv("assets/data/data.csv").then(function(demoData, err) {
         if (err) throw err;
@@ -72,8 +69,8 @@ function scatterPlot() {
         });
 
         // Create x/y linear scales.
-        var xLinearScale = xScale(demoData, chosenXAxis, chartWidth);
-        var yLinearScale = yScale(demoData, chosenYAxis, chartHeight);
+        var xLinearScale = xScale(demoData, xaxis, chartWidth);
+        var yLinearScale = yScale(demoData, yaxis, chartHeight);
 
         // Create initial axis functions.
         var bottomAxis = d3.axisBottom(xLinearScale);
@@ -83,6 +80,21 @@ function scatterPlot() {
         var xAxis = chartGroup.append("g")
             .attr("transform", `translate(0, ${chartHeight})`)
             .call(bottomAxis);
+
+        svg.append("text")
+            .attr("transform",
+                "translate(" + (width / 2) + " ," +
+                (height + margin.top + 215) + ")")
+            .style("text-anchor", "middle")
+            .text("In Poverty (%)");
+
+        // text label for the y axis
+        svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("x", 0 - (height / 1.5))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("Lacks Healthcare (%)");
 
         // Append y axis.
         var yAxis = chartGroup.append("g")
@@ -97,15 +109,15 @@ function scatterPlot() {
 
         // Create circles.
         var circle = elemEnter.append("circle")
-            .attr("cx", d => xLinearScale(d[chosenXAxis]))
-            .attr("cy", d => yLinearScale(d[chosenYAxis]))
+            .attr("cx", d => xLinearScale(d[xaxis]))
+            .attr("cy", d => yLinearScale(d[yaxis]))
             .attr("r", 15)
             .classed("stateCircle", true);
 
-        // Create circle text.
+        // Circles to plot onto the graph.
         var circleText = elemEnter.append("text")
-            .attr("x", d => xLinearScale(d[chosenXAxis]))
-            .attr("y", d => yLinearScale(d[chosenYAxis]))
+            .attr("x", d => xLinearScale(d[xaxis]))
+            .attr("y", d => yLinearScale(d[yaxis]))
             .attr("dy", ".35em")
             .text(d => d.abbr)
             .classed("stateText", true);
