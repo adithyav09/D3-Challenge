18f4c8cc1a115a31e070c76ccdce6132735af0ad
adithyavenkatesh <adithyav211@gmail.com>
2020-11-08 18:01:50 -0500

    added comments and adjusted margins

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,44 +5,36 @@ var margin = { top: 20, right: 20, bottom: 50, left: 70 },
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-// Function used for updating x-scale var upon click on axis label.
-function xScale(data, xaxis, chartWidth) {
+// Updates the x scale.
+function xScale(data, xaxis, graphWidth) {
     // Create scales.
-    var xLinearScale = d3.scaleLinear()
+    var Xlinear = d3.scaleLinear()
         .domain([d3.min(data, d => d[xaxis]) * .8,
             d3.max(data, d => d[xaxis]) * 1.1
         ])
-        .range([0, chartWidth]);
-    return xLinearScale;
+        .range([0, graphWidth]);
+    return Xlinear;
 }
 
 // Function used for updating y-scale var upon click on axis label.
-function yScale(data, yaxis, chartHeight) {
+function yScale(data, yaxis, graphHeight) {
     // Create scales.
-    var yLinearScale = d3.scaleLinear()
+    var YLinear = d3.scaleLinear()
         .domain([d3.min(data, d => d[yaxis]) * .8,
             d3.max(data, d => d[yaxis]) * 1.2
         ])
-        .range([chartHeight, 0]);
-    return yLinearScale;
+        .range([graphHeight, 0]);
+    return YLinear;
 }
 
 function scatterPlot() {
 
-    // Select div by id.
-    var svgArea = d3.select("#scatter").select("svg");
-    // Clear SVG.
-    if (!svgArea.empty()) {
-        svgArea.remove();
-    }
-
-    //SVG params.
     var svgHeight = window.innerHeight / 1.2;
     var svgWidth = window.innerWidth / 1.7;
 
-    // Chart area minus margins.
-    var chartHeight = svgHeight - margin.top - margin.bottom;
-    var chartWidth = svgWidth - margin.left - margin.right;
+    // Graph area minus margins.
+    var graphHeight = svgHeight - margin.top - margin.bottom;
+    var graphWidth = svgWidth - margin.left - margin.right;
 
     // Create an SVG wrapper, append an SVG group that will hold our chart,
     // and shift the latter by left and top margins.
@@ -56,10 +48,10 @@ function scatterPlot() {
     var chartGroup = svg.append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
-    d3.csv("assets/data/data.csv").then(function(demoData, err) {
+    d3.csv("assets/data/data.csv").then(function(data, err) {
         if (err) throw err;
-        // Parse data.
-        demoData.forEach(function(data) {
+        // Loop through the data in the CSV in order to plot the graph.
+        data.forEach(function(data) {
             data.poverty = +data.poverty;
             data.healthcare = +data.healthcare;
             data.age = +data.age;
@@ -68,19 +60,20 @@ function scatterPlot() {
             data.obesity = data.obesity;
         });
 
-        // Create x/y linear scales.
-        var xLinearScale = xScale(demoData, xaxis, chartWidth);
-        var yLinearScale = yScale(demoData, yaxis, chartHeight);
+        // Create x and y linear scale.
+        var Xlinear = xScale(data, xaxis, graphWidth);
+        var YLinear = yScale(data, yaxis, graphHeight);
 
-        // Create initial axis functions.
-        var bottomAxis = d3.axisBottom(xLinearScale);
-        var leftAxis = d3.axisLeft(yLinearScale);
+        // Initialize the x and y axis.
+        var bottomAxis = d3.axisBottom(Xlinear);
+        var leftAxis = d3.axisLeft(YLinear);
 
-        // Append x axis.
+        // Add the x axis.
         var xAxis = chartGroup.append("g")
-            .attr("transform", `translate(0, ${chartHeight})`)
+            .attr("transform", `translate(0, ${graphHeight})`)
             .call(bottomAxis);
 
+        // X axis Label
         svg.append("text")
             .attr("transform",
                 "translate(" + (width / 2) + " ," +
@@ -88,7 +81,7 @@ function scatterPlot() {
             .style("text-anchor", "middle")
             .text("In Poverty (%)");
 
-        // text label for the y axis
+        // Y axis Label
         svg.append("text")
             .attr("transform", "rotate(-90)")
             .attr("x", 0 - (height / 1.5))
@@ -96,28 +89,28 @@ function scatterPlot() {
             .style("text-anchor", "middle")
             .text("Lacks Healthcare (%)");
 
-        // Append y axis.
+        // Add the y axis.
         var yAxis = chartGroup.append("g")
             .call(leftAxis);
 
-        // Set data used for circles.
-        var circlesGroup = chartGroup.selectAll("circle")
-            .data(demoData);
+        // Add the data for each of the circles.
+        var circles = chartGroup.selectAll("circle")
+            .data(data);
 
-        // Bind data.
-        var elemEnter = circlesGroup.enter();
+        // Combine the data.
+        var elemEnter = circles.enter();
 
         // Create circles.
         var circle = elemEnter.append("circle")
-            .attr("cx", d => xLinearScale(d[xaxis]))
-            .attr("cy", d => yLinearScale(d[yaxis]))
+            .attr("cx", d => Xlinear(d[xaxis]))
+            .attr("cy", d => YLinear(d[yaxis]))
             .attr("r", 15)
             .classed("stateCircle", true);
 
         // Circles to plot onto the graph.
         var circleText = elemEnter.append("text")
-            .attr("x", d => xLinearScale(d[xaxis]))
-            .attr("y", d => yLinearScale(d[yaxis]))
+            .attr("x", d => Xlinear(d[xaxis]))
+            .attr("y", d => YLinear(d[yaxis]))
             .attr("dy", ".35em")
             .text(d => d.abbr)
             .classed("stateText", true);
@@ -126,4 +119,6 @@ function scatterPlot() {
         console.log(err);
     });
 }
+
+// Call the Scatter plot function to display.
 scatterPlot();
